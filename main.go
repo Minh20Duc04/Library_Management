f3522a2d8e61b4fbf4fcef6fa33463a80067352d
Minh20Duc04 <nguyenminhduc02042004@gmail.com>
2025-01-14 15:33:19 +0700

    More API method

--- main.go
+++ main.go
@@ -18,12 +18,37 @@ var books = []book {
 	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
 }
 
-func getBooks(c *gin.Context) {
+func getBooks(c *gin.Context) { //GET method
 	c.IndentedJSON(http.StatusOK, books)
+}
+
+func createBook(c *gin.Context) {
+	var newBook book 
+	if err := c.BindJSON(&newBook); err != nil {	//POST method
+													//BindJSON để gán dữ liệu Json bên ngoài vào struct
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook) //IndentedJSON căn lề và xuống dòng, không thì dùng JSON
+}
+
+func getBookById(c *gin.Context) {
+	id := c.Param("id")
+
+	for i, b := range books{
+		if id == b.ID{
+			c.IndentedJSON(http.StatusOK, books[i]) 
+			return
+		}
+	}
+	c.JSON(http.StatusNotFound, gin.H{"error": "Not found book with id: " + id}) // dùng gin.H là 1 map[string]any để hiển thị lỗi, + để nối chuỗi
 }
 
 func main() {
 	router := gin.Default() //tạo router để xử lý các request 
-	router.GET("/books",getBooks) //tạo route
+	router.GET("/book/getAll",getBooks)
+	router.POST("/book/createBook", createBook)
+	router.GET("book/getById/:id", getBookById)
 	router.Run("localhost:8080")
 }
